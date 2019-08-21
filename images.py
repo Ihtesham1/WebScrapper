5bcfe972ca715a791801e0db288c966479efb430
Ihtesham <qazi.m.ihtesham@gmail.com>
2019-08-21 12:33:03 +0500

    Bugs removed

--- images.py
+++ images.py
@@ -18,15 +18,16 @@ def startSearch():
     links = soup.findAll("a", {"class": "thumb"})
 
     for items in links:
-        img_obj = requests.get(items.attrs["href"])
-        print "Getting: ", items.attrs["href"]
-        title = items.attrs["href"].split("/")[-1]
-
-        urllib.urlretrieve(items.attrs["href"], os.path.join("./" +dir_name + "/", title))
-        #img = Image.open(BytesIO(img_obj.content))
-        #img.save("./" + dir_name + "/", img.format)
-
-
+        try:
+            img_obj = requests.get(items.attrs["href"])
+            print "Getting: ", items.attrs["href"]
+            title = items.attrs["href"].split("/")[-1]
+            try:
+                urllib.urlretrieve(items.attrs["href"], os.path.join("./" +dir_name + "/", title))
+            except:
+                print IOError
+        except:
+            print "Picture not found"
 
     startSearch()
 
